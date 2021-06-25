7220d62ab7bbd3da38a2d4d4fa7dab157a8d3319
0008PO744 <0008PO744@DESKTOP-6P9AFJM>
2021-06-25 12:52:21 +0530

    changed payload

--- restAssuredFramework/src/main/java/Utility/PayLoadConverter.java
+++ restAssuredFramework/src/main/java/Utility/PayLoadConverter.java
@@ -7,7 +7,8 @@ import java.nio.file.Paths;
 public class PayLoadConverter {
 	
 	public static  String generatePayLoadString(String fileName) throws IOException{
-		String filePath="C:\\Users\\DeekshaKinya\\workspace\\restAssuredFramework\\src\\test\\resources\\JsonFiles\\"+fileName;
+		//String filePath="C:\\Users\\DeekshaKinya\\workspace\\restAssuredFramework\\src\\test\\resources\\JsonFiles\\"+fileName;
+		String filePath=System.getProperty("user.dir")+"\\src\\test\\resources\\JsonFiles\\"+ fileName;        
 		return new String(Files.readAllBytes(Paths.get(filePath)));
 	}
 
